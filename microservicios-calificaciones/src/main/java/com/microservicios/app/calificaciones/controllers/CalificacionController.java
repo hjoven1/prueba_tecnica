cbead420687cf12b8c951ac596e4aafbf3f115cb
hjoven <hectorjovenmendez@gmail.com>
2020-11-01 22:02:05 -0500

    cors de calificacion

--- microservicios-calificaciones/src/main/java/com/microservicios/app/calificaciones/controllers/CalificacionController.java
+++ microservicios-calificaciones/src/main/java/com/microservicios/app/calificaciones/controllers/CalificacionController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservicios.app.calificaciones.models.entity.Calificacion;
 import com.microservicios.app.calificaciones.services.CalificacionService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 public class CalificacionController {
 
